a787263a07b0e05bd44242f91aa475528228862f
Alexandre Rabérin <kernelith@live.fr>
2015-10-28 20:18:36 +0100

    modify cmake to enable mulithread programmation

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,13 @@ int main()
     const unsigned int nbMonth = 80;
 
     Simulation simu;
+    simu.writeToFile("simu.log");
     for (unsigned int i = 0 ; i < nbMonth ; i++)
     {
-        simu.writeToFile("simu.log");
         simu.simulateNextMonth();
+        simu.writeToFile("simu.log");
         std::cout <<"Mois " << i << " => Nombre de lapin : " << simu.getNbRabbit() << std::endl;
     }
-
-    simu.writeToFile("simu.log");
 
     return 0;
 }
