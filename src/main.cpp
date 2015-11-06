959bce38933ca0ea1cd79cd41ab61c1b1ecfdf5c
Ace Nanter <ace.nanter@hotmail.fr>
2015-11-06 15:38:57 +0100

    Mise en place du taux de survie évolutif pour les jeunes lapins + TODO réflexion mise en place gaussienne : moyenne 6.5 et écart type ~0.75

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,13 @@ int main(int /*argc*/, char** /*argv*/)
 
     Simulation simu;
     simu.writeToFile("simu.log");
-    for (unsigned int i = 0 ; i < nbMonth ; i++)
+	unsigned int i = 0;
+    while(simu.getNbRabbit() < 10000000000)
     {
         simu.simulateNextMonth();
         simu.writeToFile("simu.log");
-        std::cout <<"Mois " << i+1 << " => Nombre de lapin : " << simu.getNbRabbit() << std::endl;
+        std::cout <<"Mois " << ++i << " => Nombre de lapin : " << simu.getNbRabbit() << std::endl;
+		
     }
 
     return 0;
