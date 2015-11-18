be98db04eedf759cadd57672a2316a0337731427
Alexandre Rabérin <kernelith@live.fr>
2015-11-18 23:11:16 +0100

    restaure changes

---
+++ src/index.h
@@ -0,0 +1,25 @@
+#ifndef INDEX_H
+#define INDEX_H
+
+/*!
+ * @mainpage
+ *
+ * @tableofcontents
+ *
+ *
+ * <center>Documentation de Croissance Population<br>
+ * TP de simulation 2ème année ISIMA.<br></center>
+ *
+ * @image html lapin.png
+ *
+ *
+ * @section pres Présentation
+ *
+ * Ceci est la documentation du TP de Simulation visant à simuler l'évolution d'une population de lapins de Garenne<br>
+ * Il met en jeu des problématiques de contraintes mémoire et temporelle pour la génération d'un grand nombre d'individus.<br>
+ * En effet, la reproduction des lapin amène à une croissance exponentielle ce qui fait rapidement exploser les traitements à réaliser.<br>
+ * Il convient donc de se fixer des objectifs et des simplifications dans le modèle de simulation adopté, ainsi que d'optimiser au mieux les algorithmes mis en jeu.
+ */
+
+
+#endif // INDEX_H
